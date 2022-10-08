32d6e2c71fae14c5c471a67a373c518d3f361840
reapxrsz <matheuscard1414@gmail.com>
2022-10-08 16:15:23 -0300

    commit

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Acai from './acai/acai.js';
+import Gramas from './gramas/gramas.js';
+import Libra from './libra/libra.js';
 
 export default function Index(){
     return(
         <React.StricMode>
             <BrowserRouter>
                 <Routes>
-                    <Route exact path='/' element={</>}></Route>
-                    <Route exact path='/' element={</>}></Route>
-                    <Route exact path='/' element={</>}></Route>
+                    <Route exact path='/acai' element={<Acai />}></Route>
+                    <Route exact path='/gramas' element={<Gramas />}></Route>
+                    <Route exact path='/libra' element={<Libra />}></Route>
                 </Routes>
             </BrowserRouter>
         </React.StricMode>
